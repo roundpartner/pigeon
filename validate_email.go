ffedeb40ff1892f61859c6e34b4e5df847b4b31b
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2020-07-17 02:20:34 +0100

    Update messages to be warnings

--- validate_email.go
+++ validate_email.go
@@ -9,12 +9,12 @@ import (
 func ValidateEmail(email string) bool {
 	components := strings.Split(email, "@")
 	if len(components) != 2 {
-		log.Printf("[ERROR] [%s] %s is not a valid email address", ServiceName, email)
+		log.Printf("[WARNING] [%s] %s is not a valid email address", ServiceName, email)
 		return false
 	}
 	mxrecords, err := net.LookupMX(components[1])
 	if err != nil {
-		log.Printf("[ERROR] [%s] %s", ServiceName, err.Error())
+		log.Printf("[WARNING] [%s] MX Response: %s", ServiceName, err.Error())
 		return false
 	}
 	return len(mxrecords) > 0
